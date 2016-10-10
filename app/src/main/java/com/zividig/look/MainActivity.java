c9e6dfc3ce13f3a23dd9679ddc79c095c9c09646
Adolphsa <497749990@qq.com>
2016-10-10 11:41:30 +0800

    Initial commit

---
+++ app/src/main/java/com/zividig/look/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zividig.look;
+
+import android.os.Bundle;
+
+import com.zividig.look.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/zividig/look/activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.zividig.look.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adolph
+ * on 2016-10-10.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
